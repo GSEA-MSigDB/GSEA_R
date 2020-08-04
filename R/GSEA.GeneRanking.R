1da93977b94f56c50ee2944b73126762c804889b
Anthony S. Castanza <acastanza@ucsd.edu>
2020-08-04 15:01:06 -0700

    Experimental Permutation DESeq2 Based Permutation Testing

--- R/GSEA.GeneRanking.R
+++ R/GSEA.GeneRanking.R
@@ -254,7 +254,8 @@ GSEA.GeneRanking <- function(A, class.labels, gene.labels, nperm, permutation.ty
   
   obs.rnk.matrix <- M1/S1
   gc()
- } else if (rank.metric == "ttest") {
+ }
+ if (rank.metric == "ttest") {
   # compute TTest for the random permutation matrix
   P <- reshuffled.class.labels1 * subset.mask
   n1 <- sum(P[, 1])
@@ -355,7 +356,8 @@ GSEA.GeneRanking <- function(A, class.labels, gene.labels, nperm, permutation.ty
   
   obs.rnk.matrix <- M1/S1
   gc()
- } else if (rank.metric == "seq") {
+ }
+ if (rank.metric == "seq") {
   library(DESeq2)
   coldata <- as.data.frame(colnames(A))
   rownames(coldata) <- coldata[, 1]
