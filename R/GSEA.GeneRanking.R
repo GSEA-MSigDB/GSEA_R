038c59d34e816417843dd95b1e9d23a1d6388537
Anthony S. Castanza <acastanza@ucsd.edu>
2020-08-11 15:32:50 -0700

    GSEA_R v1.3

--- R/GSEA.GeneRanking.R
+++ R/GSEA.GeneRanking.R
@@ -384,7 +384,7 @@ if (rank.metric == "S2N"|rank.metric == "ttest") {
    if (rank.metric == "signedsig") {
     rnk.matrix[, d] <- sign(res[, 2])*-log10(res[, 5]) #rank by the -log10(pValue) signed by the Log2(FC)
    }
-  }
+
 
    if (length(rnk.matrix[is.na(rnk.matrix)])>0) {
     warning(print(length(rnk.matrix[is.na(rnk.matrix)])), " N/A values were found in the permuted rank matrix. Setting N/As to Zero because these cause GSEA to fail.")
@@ -407,20 +407,20 @@ if (rank.metric == "S2N"|rank.metric == "ttest") {
   dds <- DESeq(dds)
   res <- results(dds)
    if (rank.metric == "change") {
-    obs.rnk.matrix[, nperm] <- res[, 2] #rank by Log2(FC)
+    obs.rnk.matrix[, d] <- res[, 2] #rank by Log2(FC)
    }
    if (rank.metric == "scaledchange") {
-    obs.rnk.matrix[, nperm] <- res[, 2]*-log10(res[, 5]) #rank by Log2(FC)*-log10(pValue)
+    obs.rnk.matrix[, d] <- res[, 2]*-log10(res[, 5]) #rank by Log2(FC)*-log10(pValue)
    } 
    if (rank.metric == "signedsig") {
-    obs.rnk.matrix[, nperm] <- sign(res[, 2])*-log10(res[, 5]) #rank by the -log10(pValue) signed by the Log2(FC)
+    obs.rnk.matrix[, d] <- sign(res[, 2])*-log10(res[, 5]) #rank by the -log10(pValue) signed by the Log2(FC)
    }
 
    if (length(obs.rnk.matrix[is.na(obs.rnk.matrix)])>0) {
     warning(print(length(obs.rnk.matrix[is.na(obs.rnk.matrix)]))," N/A values were found in the observed ranked list. Setting N/As to Zero because these cause GSEA to fail.")
     obs.rnk.matrix[is.na(obs.rnk.matrix)]<-0
    }
-
+  }
  }
  
  if (reverse.sign == T) {
